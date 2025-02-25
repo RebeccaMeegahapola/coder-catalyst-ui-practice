1cbcbc5860cdfa32862cd7c42c584d45af908d0b
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-02-25 16:50:17 +0530

    feat: add Header component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Colors } from "@/constants/Colors";
+import {Header} from "@/components/navbar/header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <h1 style={{ color: Colors.primary }}>e-commerce site</h1>
     </div>
   );
