e59469a5ce9a96fff3c9e55fc416a3f46be27191
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-03-08 17:45:53 +0530

    feat: add hero section component to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { Colors } from "@/constants/Colors";
 import {Header} from "@/components/navbar/header";
 import {SubNavbar} from "@/components/navbar/sub-navbar";
+import HeroSection from "@/components/sections/hero-section";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div>
       <Header />
       <SubNavbar />
+      <HeroSection />
     </div>
   );
 }
