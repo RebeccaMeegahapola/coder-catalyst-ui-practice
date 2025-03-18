a01aa2c819584863a323da41a351beaeaef3f0e3
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-03-18 12:08:14 +0530

    chore: replace Geist fonts with Roboto and remove favicon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+    variable: "--font-roboto",
+    subsets: ["latin"],
+    weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "e-Commerce Site",
@@ -24,11 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-            <link rel="icon" href="/assets/images/navbar/favicon.png" />
             <title>e-Commerce App</title>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} overflow-x-hidden`}
       >
         {children}
       </body>
