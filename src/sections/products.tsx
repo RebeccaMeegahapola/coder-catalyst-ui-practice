599ab4a82502b46bf3a77f3d595579605f2fffe1
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-03-29 12:57:31 +0530

    feat: add `descriptionMobile` to product data for responsive design

--- src/sections/products.tsx
+++ src/sections/products.tsx
@@ -3,28 +3,29 @@ import ProductCard from "@/components/products/product-card";
 export default function Products() {
 
     const Products = [
-        {image: "/assets/images/products/Iphone 14 pro.png", alt: "Apple iPhone 14 Pro Max", label: "Apple iPhone 14 Pro Max 128GB", description: "Deep Purple (MO9T3RX/A)", price: "$900"},
-        {image: "/assets/images/products/Camera.png", alt: "Blackmagic Pocket Cinema", label: "Blackmagic Pocket Cinema", description: "Camera 6k", price: "$2535"},
-        {image: "/assets/images/products/applewatch.png", alt: "Apple Watch Series 9", label: "Apple Watch Series 9 GPS 41mm", description: "Starlight Aluminium Case", price: "$399"},
-        {image: "/assets/images/products/airpods.png", alt: "AirPods Max Silver", label: "AirPods Max Silver", description: "", price: "$549"},
-        {image: "/assets/images/products/watch.png", alt: "Samsung Galaxy Watch6 Classic", label: "Samsung Galaxy Watch6 Classic", description: "47mm Black", price: "$369"},
-        {image: "/assets/images/products/galaxyZ.png", alt: "Galaxy Z Fold5 Unlocked | 256GB", label: "Galaxy Z Fold5 Unlocked | 256GB", description: "| Phantom Black", price: "$1799", isFavourite: true},
-        {image: "/assets/images/products/galaxyBuds.png", alt: "Galaxy Buds FE", label: "Galaxy Buds FE", description: "Graphite", price: "$99.99"},
-        {image: "/assets/images/products/ipad.png", alt: "Apple iPad 9", label: "Apple iPad 9 10.2\" 64GB Wi-Fi", description: "Silver (MK2L3) 2021", price: "$398"},
+        {image: "/assets/images/products/Iphone 14 pro.png", alt: "Apple iPhone 14 Pro Max", label: "Apple iPhone 14 Pro Max 128GB", description: "Deep Purple (MO9T3RX/A)", descriptionMobile: "Apple iPhone 14 Pro Max 128GB Deep Purple (MQ9T3RX/A)", price: "$900"},
+        {image: "/assets/images/products/Camera.png", alt: "Blackmagic Pocket Cinema", label: "Blackmagic Pocket Cinema", description: "Camera 6k", descriptionMobile: "Blackmagic Pocket Cinema Camera 6k", price: "$2535"},
+        {image: "/assets/images/products/applewatch.png", alt: "Apple Watch Series 9", label: "Apple Watch Series 9 GPS 41mm", description: "Starlight Aluminium Case", descriptionMobile: "Apple Watch Series 9 GPS 41mm Starlight Aluminium Case", price: "$399"},
+        {image: "/assets/images/products/airpods.png", alt: "AirPods Max Silver", label: "AirPods Max Silver", description: "", descriptionMobile: "AirPods Max Silver", price: "$549"},
+        {image: "/assets/images/products/watch.png", alt: "Samsung Galaxy Watch6 Classic", label: "Samsung Galaxy Watch6 Classic", description: "47mm Black", descriptionMobile: "Samsung Galaxy Watch6 Classic 47mm Black", price: "$369"},
+        {image: "/assets/images/products/galaxyZ.png", alt: "Galaxy Z Fold5 Unlocked | 256GB", label: "Galaxy Z Fold5 Unlocked | 256GB", description: "| Phantom Black", descriptionMobile: "Galaxy Z Fold5 Unlocked | 256GB | Phantom Black", price: "$1799", isFavourite: true},
+        {image: "/assets/images/products/galaxyBuds.png", alt: "Galaxy Buds FE", label: "Galaxy Buds FE", description: "Graphite", descriptionMobile: "Galaxy Buds FE\n" +
+                "Graphite", price: "$99.99"},
+        {image: "/assets/images/products/ipad.png", alt: "Apple iPad 9", label: "Apple iPad 9 10.2\" 64GB Wi-Fi", description: "Silver (MK2L3) 2021", descriptionMobile: "Apple iPad 9 10.2\" 64GB Wi-Fi Silver (MK2L3) 2021", price: "$398"},
     ]
 
     return (
         <div className="bg-white min-w-full py-20">
             <div className='container mx-auto px-4 md:px-20 lg:max-w-[1000px] 2xl:max-w-[1300px]'>
                 {/* Tabs Section */}
-                <div className="flex justify-start gap-8 mb-8">
-                  <span className="text-lg font-medium underline decoration-2 decoration-black underline-offset-4 text-black">
+                <div className="flex justify-start gap-4 sm:gap-8 mb-8">
+                  <span className="text-sm md:text-lg font-medium underline decoration-2 decoration-black underline-offset-4 text-black">
                     New Arrival
                   </span>
-                  <span className="text-lg font-medium text-[#8B8B8B]">
+                  <span className="text-sm md:text-lg font-medium text-[#8B8B8B]">
                     Bestseller
                   </span>
-                  <span className="text-lg font-medium text-[#8B8B8B]">
+                  <span className="text-sm md:text-lg font-medium text-[#8B8B8B]">
                     Featured Products
                   </span>
                 </div>
