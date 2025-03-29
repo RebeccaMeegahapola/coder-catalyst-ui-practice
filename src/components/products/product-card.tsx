f6579dc74412610ad0d665a34c83ffdec517084e
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-03-29 13:03:33 +0530

    feat: add `descriptionMobile` to Product data for responsive design

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -6,6 +6,7 @@ interface Product {
     alt: string;
     label: string;
     description: string;
+    descriptionMobile: string;
     price: string;
     isFavourite?: boolean;
 }
@@ -18,7 +19,7 @@ export default function ProductCard({ product }: ProductCardProps) {
 
     return (
         <div className="max-w-sm rounded-[10px] overflow-hidden shadow-sm bg-[#F6F6F6]">
-            <div className="flex justify-end mt-8 mr-6">
+            <div className="flex justify-end mt-8 mr-6 mb-4 sm:mb-0">
                 {product.isFavourite ?
                     <HeartIcon fill="#FF0000" className="h-8 w-8 cursor-pointer text-[#FF0000]" strokeWidth={1}/>
                 :
@@ -32,18 +33,24 @@ export default function ProductCard({ product }: ProductCardProps) {
                     alt={product.label}
                     width={160}
                     height={160}
-                    className="mb-4 w-[160px] h-[160px]"
+                    className="mb-4 w-[100px] h-[100px] sm:w-[160px] sm:h-[160px]"
                 />
-                <div className="font-medium text-base text-black">{product.label}</div>
-                <p className="font-medium text-base text-black mb-2">
-                    {product.description || '\u00A0'}
-                </p>
+                <div className="hidden lg:block font-medium text-base text-black">{product.label}</div>
+                <div className="min-h-[70px] lg:min-h-[40px] w-full flex items-center justify-center">
+                    <p className="hidden lg:block font-medium text-base text-black mb-2 text-center">
+                        {product.description || '\u00A0'}
+                    </p>
+                    <p className="block lg:hidden font-medium text-sm text-black mb-2 text-center">
+                        {product.descriptionMobile}
+                    </p>
+                </div>
                 <span
-                    className="text-2xl font-semibold text-black mb-2">
+                    className="text-base sm:text-2xl font-semibold text-black mb-2">
                     {product.price}
                 </span>
-                <div className="px-6 pt-4 pb-2">
-                    <button className="bg-black hover:bg-black text-white font-medium text-sm py-3 px-14 rounded-[8px]">
+                <div className="px-4 sm:px-6 pt-4 pb-2">
+                    <button
+                        className="bg-black hover:bg-black text-white font-medium text-xs sm:text-sm py-3 px-5 md:px-8 lg:px-14 rounded-[8px] w-full">
                         Buy Now
                     </button>
                 </div>
